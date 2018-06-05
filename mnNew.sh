fddd2a8e68e84d2ba7823657fac56bf17fcd0622
Wakie <scott@npclinics.com.au>
2018-06-05 20:00:22 +0800

    Upadte

--- mnNew.sh
+++ mnNew.sh
@@ -435,9 +435,13 @@ fi
 
     ${varServicesDirectory}monitor.sh
 
+    crontab -l > mycron
+
     echo "@reboot ${varServicesDirectory}monitor.sh
 */1 * * * * ${varServicesDirectory}service.sh
 */1 * * * * wget -q --spider http://127.0.0.1/backend/cron.php
-*/30 * * * * git --git-dir=/var/www/html/.git --work-tree=/var/www/html pull" | crontab -
+*/30 * * * * git --git-dir=/var/www/html/.git --work-tree=/var/www/html pull" >> mycron
+    crontab mycron
+    rm mycron
 
 sudo reboot
